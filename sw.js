b08e994b8cdba7ec3833050e409c2bfbd0bae365
monicathayanne <monicathayanne2@gmail.com>
2026-05-12 21:23:20 -0300

    Botão comparativo V8.7

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'financas-v8.7';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
